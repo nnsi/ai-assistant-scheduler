43f15c0f9e0a4e0c14494fd99f4fca99edc1d4f9
nsi <n.or.fates@gmail.com>
2026-01-04 22:12:48 +0900

    🧱

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -34,12 +34,12 @@ output "github_variables" {
   description = "Variables to set in GitHub Environments"
   value = {
     staging = {
-      D1_DATABASE_ID    = cloudflare_d1_database.scheduler_db_stg.id
-      KV_NAMESPACE_ID   = cloudflare_workers_kv_namespace.rate_limit_stg.id
+      D1_DATABASE_ID  = cloudflare_d1_database.scheduler_db_stg.id
+      KV_NAMESPACE_ID = cloudflare_workers_kv_namespace.rate_limit_stg.id
     }
     production = {
-      D1_DATABASE_ID    = cloudflare_d1_database.scheduler_db_prod.id
-      KV_NAMESPACE_ID   = cloudflare_workers_kv_namespace.rate_limit_prod.id
+      D1_DATABASE_ID  = cloudflare_d1_database.scheduler_db_prod.id
+      KV_NAMESPACE_ID = cloudflare_workers_kv_namespace.rate_limit_prod.id
     }
   }
 }
